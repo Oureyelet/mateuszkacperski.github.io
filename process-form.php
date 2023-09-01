37def3c513e6f5e4c9ff96b9a5bc8cbffdf149df
Mateusz Kacperski <oureyelet@gmail.com>
2023-09-01 05:24:01 +0100

    Contact me updated

--- process-form.php
+++ process-form.php
@@ -12,6 +12,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mailBody .= "Email: $email\n";
     $mailBody .= "Message:\n$message";
     
-    mail($to, $subject, $mailBody, $headers);
+    if (mail($to, $subject, $mailBody, $headers)) {
+        echo "Thank you for your message. We'll get back to you shortly.";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
 }
 ?>
+
