947aa31fd548d461453d01ba52b91200a2f2d9cd
Mateusz Kacperski <oureyelet@gmail.com>
2023-12-09 19:44:07 +0000

    New portfolio in life ! :)

--- script.js
+++
@@ -1,9 +0,0 @@
-const fontSizeRange = document.getElementById('font-size-range');
-const fontSizeValue = document.getElementById('font-size-value');
-const textToChange = document.getElementById('text-to-change');
-
-fontSizeRange.addEventListener('input', function () {
-    const fontSize = this.value + 'px';
-    textToChange.style.fontSize = fontSize;
-    fontSizeValue.textContent = fontSize;
-});
